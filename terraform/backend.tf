8c0f1b37c8a21e17ee7edce592ee88f5d1dfe36b
nouraldeen417 <nouraldeennabil259@gmail.com>
2026-08-01 17:13:59 +0300

    first commit

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,5 +4,6 @@ terraform {
     storage_account_name = "sttfstate152"
     container_name       = "tfstate"
     key                  = "flaskapp.tfstate"
+    use_oidc             = true  # Authenticate using the GitHub OIDC token instead of access keys
   }
 }
