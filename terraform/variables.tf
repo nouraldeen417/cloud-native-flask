3636c996e30b38999bb11f831308c3028511c7e5
nouraldeen417 <nouraldeennabil259@gmail.com>
2026-08-02 17:12:19 +0300

    first commit

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -58,7 +58,7 @@ variable "aks_node_count" {
 variable "aks_vm_size" {
   description = "VM size for AKS nodes"
   type        = string
-  default     = "Standard_B2s"
+  default     = "Standard_B2s_v2"
 }
 
 /*--------------------------Ingress---------------------------*/
