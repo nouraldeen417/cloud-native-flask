8f382a3ad7ccf7380de80ccc739eb3b9a49623ab
nouraldeen417 <nouraldeennabil259@gmail.com>
2026-08-07 15:58:45 +0300

    multi cloud azure & aws

--- terraform/aws/eks.tf
+++ terraform/aws/eks.tf
@@ -44,3 +44,8 @@ resource "kubernetes_secret" "mysql_secret" {
 
   depends_on = [aws_eks_cluster.main]
 }
+provider "kubernetes" {
+  host                   = aws_eks_cluster.main.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.main.token
+}
